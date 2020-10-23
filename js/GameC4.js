fb07307c8e7beba36f5ab80c50793e89089c1a13
Fatake <pace_lozano@hotmail.com>
2020-10-22 21:39:53 -0500

    Cambios en estructura de variables

--- js/GameC4.js
+++ js/GameC4.js
@@ -1,48 +1,53 @@
 /**
  * Variables de configuracion
  */
-// Canvas
+/** 
+ * 
+ * Configuracion Canvas 
+ * 
+ */
 var ctx, canvas;
-
-var tiradas, gameOver;
-
-// Arreglos del juego
-var fichas_array;
-var conectaMatrix = [];
-
-// Nombre del Canvas
-var TABLERO_NAME = "conecta4Tablero";
+const TABLERO_NAME = "conecta4Tablero";// Nombre del Canvas
 
 // Color 
 var colorGato   = "black";
 var colorCanvas = "black";
-
-var fichas_X    = 0;
-var fichas_O    = 0;
-
-var largo       = 120;
 
 // Tamaño del Canvas
 var ancho       = 640;
 var alto        = 480;
 
 // Finlas y columnas del canvas
-var columnas = 6;
-var filas = 5;
+const columnas = 8;
+const filas = 8;
 
 // Variable que controla cada cuando se pinta una ralla
 var xDif = (ancho/columnas)-0.6; // 79.4;
 var yDif = (alto/filas)-0.6; // 59.4;
 
 /**
+ * 
+ * Configuracion del Juego
+ * 
+ */
+// Arreglos del juego
+var fichas_array;
+var conectaMatrix = [];
+
+// Constantes de la matriz
+const SIZE_MATRIX_X = columnas;
+const SIZE_MATRIX_Y = filas;
+
+// Fichas
+var fichas_X    = 0;
+var fichas_O    = 0;
+
+var tiradas, gameOver;
+
+/**
  * Funcion que inicia todo 
  */
 function iniciar(){
-    // Se crea la matriz
-    for(let i=0; i<8; i++) {
-        conectaMatrix[i] = new Array(8);
-    }
-
     // Optiene el canvas
     canvas        = document.getElementById(TABLERO_NAME);
 
@@ -86,69 +91,69 @@ function pintaTablero(){
 }
 
 function inicializaMatrix(){
-	let movX=0;
-	let movY=0;
-	
-	for(let i=0;i<8;i++){
-    	for(let j=0;j<8;j++){
+	let movX = 0;
+	let movY = 0;
+	// Se crea la matriz
+    for(let i=0; i<SIZE_MATRIX_X; i++) {
+        conectaMatrix[i] = new Array(SIZE_MATRIX_Y);
+    }
+
+    // llena la matriz en 0s
+	for(let i=0; i<SIZE_MATRIX_X; i++){
+    	for(let j=0; j<SIZE_MATRIX_Y; j++){
     		conectaMatrix[i][j] = 0;
     	}
     }
-    return false;
+    return true;
 }
 
+function ObtenerCoords(event){
+    var x = new Number();
+    var y = new Number();
+    //     var canvas = document.getElementById("canvas_1");
 
- function ObtenerCoords(event){
-        var x = new Number();
-        var y = new Number();
-   //     var canvas = document.getElementById("canvas_1");
- 
-        if (event.x != undefined && event.y != undefined){
-          x = event.x;
-          y = event.y;
-        }else{// Firefox
-          x = event.clientX + document.body.scrollLeft +
-              document.documentElement.scrollLeft;
-          y = event.clientY + document.body.scrollTop +
-              document.documentElement.scrollTop;
-        }
-        
-        
-        x -= canvas.offsetLeft;
-        y -= canvas.offsetTop;
-        
-                 return {
-                     x,y
-                 }
-        
-      }
-      tamC=1;
-      function tache(){
+    if (event.x != undefined && event.y != undefined){
+        x = event.x;
+        y = event.y;
+    }else{// Firefox
+        x = event.clientX + document.body.scrollLeft +
+        document.documentElement.scrollLeft;
+        y = event.clientY + document.body.scrollTop +
+        document.documentElement.scrollTop;
+    }
 
-                ctx.beginPath();
-                
-                ctx.moveTo(px, py);
-                ctx.lineTo(px+100,py+100);
-                ctx.stroke();
-            }
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+    return {
+        x,y
+    }
 
-            function circulo(){
+}
 
-                ctx.beginPath();
-                ctx.arc((px*tamC)+(tamC*.5),(py*tamC)+(tamC*.5),tamC*.4,0,2*Math.PI);
-                ctx.stroke();
-                
-            }
+tamC=1;
+
+function tache(){
+    ctx.beginPath();
+    ctx.moveTo(px, py);
+    ctx.lineTo(px+100,py+100);
+    ctx.stroke();
+}
+
+function circulo(){
+    ctx.beginPath();
+    ctx.arc((px*tamC)+(tamC*.5),(py*tamC)+(tamC*.5),tamC*.4,0,2*Math.PI);
+    ctx.stroke();
+}
             
 
 function posicion(){
-                var pos=ObtenerCoords(event);
-                pos.x-=350;
-                pos.y-=150;
-                px = Math.trunc(pos.x/tamC);
-                py = Math.trunc(pos.y/tamC);
-                tache();
-                circulo();
-            }
+    var pos=ObtenerCoords(event);
+    pos.x-=350;
+    pos.y-=150;
+    px = Math.trunc(pos.x/tamC);
+    py = Math.trunc(pos.y/tamC);
+    tache();
+    circulo();
+}
 
 iniciar();
